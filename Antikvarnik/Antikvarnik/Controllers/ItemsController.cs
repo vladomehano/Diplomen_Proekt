dd81a57642efc2e432dacb372a26cb5b6c08c13e
vlado <vladimir.danchev@pgmett.com>
2026-01-22 18:45:26 +0200

    Added project

---
+++ Antikvarnik/Antikvarnik/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Antikvarnik.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
