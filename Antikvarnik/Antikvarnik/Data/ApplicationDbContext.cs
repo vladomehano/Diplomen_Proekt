901c5c01701288c67c5df995f3ba58d8673485b0
vlado <vladimir.danchev@pgmett.com>
2026-01-23 15:39:30 +0200

    Added Models/Item.cs and added Initial Migration

--- Antikvarnik/Antikvarnik/Data/ApplicationDbContext.cs
+++ Antikvarnik/Antikvarnik/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Antikvarnik.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Antikvarnik.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<Item> Items { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
