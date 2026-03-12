bda7711a766cdf8ea90407e05ba76362a71c342b
Georgi <room6@gmail.com>
2026-03-12 12:36:38 +0200

    fixed errors

--- Antikvarnik/Antikvarnik/Models/Item.cs
+++ Antikvarnik/Antikvarnik/Models/Item.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Antikvarnik.Models
 {
     public class Item
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
-        public string MainPicUrl { get; set; }
-        public string PicturesUrls { get; set; }
+        public string? MainPicUrl { get; set; }
+        public string? PicturesUrls { get; set; }
 
-        
         public bool IsDeleted { get; set; }
 
         public string[] Pictures => string.IsNullOrWhiteSpace(PicturesUrls)
