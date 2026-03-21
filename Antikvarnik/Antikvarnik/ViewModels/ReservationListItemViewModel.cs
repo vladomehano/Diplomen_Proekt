9d9eba9972e3ea58f6ee60145cbaeae18f2bb3a3
vlado <vladimir.danchev@pgmett.com>
2026-03-21 19:27:01 +0200

    made the reservation logic - reserved/sold/canceled

---
+++ Antikvarnik/Antikvarnik/ViewModels/ReservationListItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Antikvarnik.Models.Enums;
+
+namespace Antikvarnik.ViewModels
+{
+    public class ReservationListItemViewModel
+    {
+        public int ReservationId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; } = string.Empty;
+
+        public string ItemImageUrl { get; set; } = string.Empty;
+
+        public decimal ItemPrice { get; set; }
+
+        public ItemStatus ItemStatus { get; set; }
+
+        public ReservationStatus ReservationStatus { get; set; }
+
+        public DateTime ReservedOn { get; set; }
+
+        public DateTime? ProcessedOn { get; set; }
+
+        public string UserEmail { get; set; } = string.Empty;
+    }
+}
